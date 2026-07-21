8aa1af08e9cee26b4936b976e386e6c069fb4b58
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-07-21 13:56:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask,request,jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")   
 def home():
-  return"Project Atlas is running!"
+  return "Project Atlas is runing!" 
 
 @app.route("/ask",methods=["POST"])
 def ask():
-  data=request.get_json()
-  question = data.get("question","")
+  data = request.get_json()
+  question = data.get("quesion","")
 
 return jsonify({
-  "gemini":"Gemini recevied:"+question,
-  "groq":"Groq recevied:"+question,
-  "deepseek":"DeepSeek received:"+question
+  "gemini":"Gemini received:" + question,
+  "grq":"Groq recevied:"+ question,
+  "deepseek":"DeepSeek recevied:" + question
 })
 
-if __name__=="__main__":
+if __name__ == "__main__":
   app.run(host="0.0.0.0",port=5000)
+  
+  
 
   
