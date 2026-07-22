c9bf03974cadd2c6f632574aff2e7a1fa63fb5c5
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-07-22 16:57:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -14,27 +14,25 @@ def home():
 
 @app.route("/ask",methods=["POST"])
 def ask():
-   data = request.get_json(force=True)
-   question = data["question"]
+  data = request.get_json(force=True)
+  question = data["question"]
 
-   gemini = requests.post(
+  gemini = requests.post(
 
-  f"https://generativelanguage.googlrapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_KEY}",
-     json={
-       "contents": [
-         {
-           "parts":[
-             {"text":question}
-           ]
-         }
-       ]
-     }
-   ).json()
-
+    f"htps://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_KEY}",
+    json={
+      "contents":[
+        {
+          "parts":[
+            {"text":question}
+          ]
+        }
+      ]
+    }
+  ).json()
   answer = gemini["candidates"][0]["content"]["parts"][0]["text"]
-
   return jsonify({
-    "answer":answer
+  "answer": answer
   })
 
 if __name__=="__main__":
