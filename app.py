558dd77175cc0b3bf12c4e0983e9dee7c65197c6
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-07-22 12:08:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ f"https:/generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:genera
     }
  ).json()
   
-  answer = gemini["candidates"][0]["content"]["parts"][0]["text"]
+  answer = gemini["candidates"][0]
+["content"]["parts"][0]["text"]
 
   return jsonify({
     "answer":answer
