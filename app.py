2e8744e347b84a008f1a8b0ea82f6658e43a7e77
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-07-26 12:33:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ def ask():
       }
     ]
   }
- 
-  else: 
-  url = "https://api.groq.com/openai/v1/chat/completions"
+
+else: 
+url = "https://api.groq.com/openai/v1/chat/completions"
 
 headers = {
   "Authorization": f"Bearer {GROQ_KEY}",
