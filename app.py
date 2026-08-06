8f2f1e894d2f05f10d5d03e3c6fccbfba96a4db3
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-08-06 12:25:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def ask_groq(question):
 # GEMINI (UPDATED)
 # ==========================
 def ask_gemini(question):
-    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent"
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent"
 
     headers = {
         "Content-Type": "application/json",
