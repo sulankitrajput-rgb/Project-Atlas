c87b8a3e1dc65b99c1c415a62621949f1be33ac0
sulankitrajput-rgb <sulankit.rajput@gmail.com>
2026-08-20 12:48:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def ask_gemini(question, image=""):
 
             image_response = requests.get(
                 image,
-                timeout=60
+                timeout=20
             )
 
             image_response.raise_for_status()
